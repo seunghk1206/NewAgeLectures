baedb1fa6b6cbbba7ca1625ffefe34e6daff8a2d
Seunghyeon Kim <60473607+seunghk1206@users.noreply.github.com>
2021-09-03 17:06:53 +0900

    Create Introduction.py

---
+++ Introduction.py
@@ -0,0 +1,58 @@
+#node js -> backend
+#Backend 80% -> API 전달, 수정, 만듬
+
+#API -> 프론트와 백엔드와의 오작교(Bridge)
+# -> 클라이언트 사이드 (프론트 엔드 유저와의 밀접한 관계성을 가지는 개발업)
+# -> REST API 방식 규칙:
+'''
+JSON(90%)
+MYSQL DATABASE JSON -> 지원 x -> 이제는 지원
+Mongo Data Base 2016 DBMS -> 안할꺼임
+'''
+'''
+Backend Framework
+Fast API
+Django
+Flask
+Node JS -> 안 배움
+v8 런타임
+node js -> v8기반
+node js 속력 증가
+
+모두 능가하는 애가 나옴
+Fast API -> 매우 압도적, 사용이 편리함
+ -> Open API swagger API 문서 자동완성
+AI 가능해짐
+기술 스텍:
+React
+Fast API
+Github
+Swagger
+MYSQL Database (RDBMS)
+AWS Hosting
+AWS RDS
+AWS S3
+
+사업 모델 정하기
+프로토타입 하는법 (Figma)
+Database UML Diagram 작성
+
+API 문서 작성 / 백엔드 백본 코드 작성 - Swagger 을 사용함
+#1
+
+AWS Hosting, AWS RDS, AWS S3 회원가입 및 결제 정보 작성
+MySQL DB에 UML Diagram 그대로 입력
+Frontend 업무 끝날 시
+배포 시작 -> AWS Hosting or Heroku Hosting 사용
+-------------------------------------------
+클라이언트 사이드 웹뷰 생성 및 뼈대 맞추기
+Request 시 API 콜 만들기
+마지막 디자인 정리 및 배포
+'''
+# ? query
+# / path
+# similar to FLASK; Pypi -> API 구성 -> 저장 매체 앱으로 배포
+
+'''
+terminal
+'''
